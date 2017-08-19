763215426abd289af3f44cade4498fbbd6777126
kennethchangg <kennethchang92@gmail.com>
2017-08-19 01:41:53 -0700

    Update assignment2.js

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,14 +1,18 @@
+
 /*
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
+
+for (var i=1;i<=100;i++) {
+        console.log(i);
+}
 
 
 
 /*
 Exercise #2
 Write a loop that makes seven calls to console.log to output the following triangle:
-
 #
 ##
 ###
@@ -17,3 +21,8 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+for (var i=1;i<=7;i++)  {
+var str = "#";
+console.log(str.repeat(i));
+}
